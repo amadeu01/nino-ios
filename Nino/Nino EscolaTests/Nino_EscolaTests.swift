a42496f191572191d4cd260a021988ded9ffd8b0
danilobecke <danilobecke@MacBook-Pro-de-Danilo-2.local>
2016-05-19 12:38:40 -0300

    Project targets 'Nino' and 'Nino Escola' created

---
+++ Nino/Nino EscolaTests/Nino_EscolaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Nino_EscolaTests.swift
+//  Nino EscolaTests
+//
+//  Created by Danilo Becke on 19/05/16.
+//  Copyright © 2016 Danilo Becke. All rights reserved.
+//
+
+import XCTest
+@testable import Nino_Escola
+
+class Nino_EscolaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
