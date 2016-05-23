9f30b752088e7ecb3cab6ab245a4bb00187cbf56
Alfredo Cavalcante Neto <alfredocavalcanteneto@Alfredos-MacBook-Pro.local>
2016-05-23 15:16:50 -0300

    Configured linter. There is still More work to be done, like setting the maximum number of whitespaces to 1.

--- Nino/Nino EscolaUITests/Nino_EscolaUITests.swift
+++ Nino/Nino EscolaUITests/Nino_EscolaUITests.swift
@@ -7,13 +7,14 @@
 //
 
 import XCTest
-
+// Disables sswiftlint, so that the underline does not cause an error
+// swiftlint:disable:next type_name
 class Nino_EscolaUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // Put  setup code here. This method is called before the invocation of each test method in the class.
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
