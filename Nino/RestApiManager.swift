3cfd10df9bf8f574443e5ff0cafff2a18951b354
danilobecke <danilobecke@MacBook-Pro-de-Danilo-2.local>
2016-07-05 12:55:17 -0300

    started to do the server integration. Confirm password token being recived

--- Nino/RestApiManager.swift
+++ Nino/RestApiManager.swift
@@ -7,7 +7,26 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class RestApiManager: NSObject {
 
+    private static let baseURL = "api.ninoapp.com.br/"
+    
+    static func makeHTTPGetRequest(path: String, onCompletion: ServiceResponse) {
+        let request = NSMutableURLRequest(URL: NSURL(string: path)!)
+        request.HTTPMethod = "GET"
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            if let jsonData = data {
+                let json = JSON(data: jsonData)
+                onCompletion(json: json, error: error)
+            } else {
+                onCompletion(json: nil, error: error)
+            }
+        }
+        task.resume()
+    }
+    
+    
 }
