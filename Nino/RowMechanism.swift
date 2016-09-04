2c50fdceb45af89ba9f21506998536a69ec0d6a5
Alfredo <alfredo.cnt@gmail.com>
2016-09-04 15:54:59 +0200

    i18n almost there

--- Nino/RowMechanism.swift
+++ Nino/RowMechanism.swift
@@ -13,26 +13,26 @@ class RowMechanism: NSObject {
     static func getRowsWithID(id: Int) -> [String: AnyObject] {
         switch id {
         case 0:
-            return ["cells": [["type": CellType.Intensity.rawValue, "title": "Lanche da Manhã", "buttons": self.food()]], "description": "No lanche da manhã eu %1.item.", "emptyDescription": ""]
+            return ["cells": [["type": CellType.Intensity.rawValue, "title": NSLocalizedString("MEAL_BREAKFAST", comment: ""), "buttons": self.food()]], "description": NSLocalizedString("DESC_BREAKFAST", comment: ""), "emptyDescription": ""]
         case 1:
-            return ["cells": [["type": CellType.Intensity.rawValue, "title": "Almoço", "buttons": self.food()]], "description": "No almoço eu %1.item.", "emptyDescription": ""]
+            return ["cells": [["type": CellType.Intensity.rawValue, "title": NSLocalizedString("MEAL_LUNCH", comment: ""), "buttons": self.food()]], "description": NSLocalizedString("DESC_LUNCH", comment: ""), "emptyDescription": ""]
         case 2:
-            return ["cells": [["type": CellType.Intensity.rawValue, "title": "Lanche da Tarde", "buttons": self.food()]], "description": "No lanche da tarde eu %1.item.", "emptyDescription": ""]
+            return ["cells": [["type": CellType.Intensity.rawValue, "title": NSLocalizedString("MEAL_AFTERNOON_SNACK", comment: ""), "buttons": self.food()]], "description": NSLocalizedString("DESC_AFTERNOON_SNACK", comment: ""), "emptyDescription": ""]
         case 3:
-            return ["cells": [["type": CellType.Intensity.rawValue, "title": "Jantar", "buttons": self.food()]], "description": "No jantar eu %1.item.", "emptyDescription": ""]
+            return ["cells": [["type": CellType.Intensity.rawValue, "title": NSLocalizedString("MEAL_DINNER", comment: ""), "buttons": self.food()]], "description": NSLocalizedString("DESC_DINNER", comment: ""), "emptyDescription": ""]
         case 4:
-            return ["cells": [["type": CellType.Slider.rawValue, "title": "Mamadeira", "floor": 0, "ceil": 200, "unity": "ml", "image": MyDaySliderIcon.BabyBottles.rawValue]], "description": "Hoje eu mamei %1.count mamadeira(s).<each> Uma de %1.item.</each>", "emptyDescription": "Hoje eu não mamei."]
+            return ["cells": [["type": CellType.Slider.rawValue, "title": NSLocalizedString("TITLE_BABY_BOTTLE", comment: ""), "floor": 0, "ceil": 200, "unity": "ml", "image": MyDaySliderIcon.BabyBottles.rawValue]], "description": NSLocalizedString("DESC_BABY_BOTTLE", comment: ""), "emptyDescription": "Hoje eu não mamei."]
         case 5:
-            return ["cells": [["type": CellType.Intensity.rawValue, "title": "Quando", "buttons": self.sleep()], ["type": CellType.Slider.rawValue, "title": "Tempo Dormido", "floor": 0, "ceil": 180, "unity": "minutos", "image": MyDaySliderIcon.BabySleep.rawValue]], "description": "Hoje eu dormi %1.count vez(es).<each> Dormi %2.item durante a %1.item.</each>", "emptyDescription": "Hoje eu não dormi."]
+            return ["cells": [["type": CellType.Intensity.rawValue, "title": NSLocalizedString("WHEN", comment: ""), "buttons": self.sleep()], ["type": CellType.Slider.rawValue, "title": NSLocalizedString("TITLE_BABY_SLEEP", comment: ""), "floor": 0, "ceil": 180, "unity": NSLocalizedString("UNIT_BABY_SLEEP", comment: ""), "image": MyDaySliderIcon.BabySleep.rawValue]], "description": NSLocalizedString("DESC_BABY_SLEEP", comment: ""), "emptyDescription": NSLocalizedString("EMPTY_DESC_BABY_SLEEP", comment: "")    ]
         case 6:
-            return ["cells": [["type": CellType.Intensity.rawValue, "title": "Evacuação", "buttons": self.evacuation()]], "description": "Hoje %1.item.", "emptyDescription": "Hoje não evacuei."]
+            return ["cells": [["type": CellType.Intensity.rawValue, "title": NSLocalizedString("TITLE_EVACUATION", comment: "") , "buttons": self.evacuation()]], "description": NSLocalizedString("DESC_EVACUATION", comment: ""), "emptyDescription": NSLocalizedString("EMPTY_DESC_EVACUATION", comment: "")]
         case 7:
-            return ["cells": [["type": CellType.Intensity.rawValue, "title": "Troca de Fralda", "buttons": self.diaper()]], "description": "Hoje %1.item.", "emptyDescription": "Hoje não troquei de fralda."]
+            return ["cells": [["type": CellType.Intensity.rawValue, "title": NSLocalizedString("TITLE_DIAPER_CHANGE", comment: ""), "buttons": self.diaper()]], "description": NSLocalizedString("DESC_DIAPER_CHANGE", comment: ""), "emptyDescription": NSLocalizedString("EMPTY_DESC_DIAPER_CHANGE", comment: "")]
             //TODO: commentaries cell
 //        case 8:
 //            return
         default:
-            return ["cells": [["type": CellType.Intensity.rawValue, "title": "Lanche da Manhã", "buttons": self.food()]], "description": "No lanche da manhã eu %1.item.", "emptyDescription": ""]
+            return ["cells": [["type": CellType.Intensity.rawValue, "title": NSLocalizedString("MEAL_BREAKFAST", comment: ""), "buttons": self.food()]], "description": NSLocalizedString("DESC_BREAKFAST", comment: ""), "emptyDescription": ""]
         }
     }
     
@@ -42,33 +42,33 @@ class RowMechanism: NSObject {
     
     private static func food() -> [[String: String]] {
         var dict = [[String: String]]()
-        dict.append(self.getButton("bem", preffix: "comi ", suffix: ""))
-        dict.append(self.getButton("pouco", preffix: "comi ", suffix: ""))
-        dict.append(self.getButton("não comi", preffix: "", suffix: ""))
+        dict.append(self.getButton(NSLocalizedString("TITLE_FOOD_WELL", comment: ""), preffix: NSLocalizedString("PREFIX_FOOD_WELL", comment: ""), suffix: NSLocalizedString("SUFFIX_FOOD_WELL", comment: "")))
+        dict.append(self.getButton(NSLocalizedString("TITLE_FOOD_LITTLE", comment: ""), preffix: NSLocalizedString("PREFIX_FOOD_LITTLE", comment: ""), suffix: NSLocalizedString("SUFFIX_FOOD_LITTLE", comment: "")))
+        dict.append(self.getButton(NSLocalizedString("TITLE_FOOD_NOTHING", comment: ""), preffix: NSLocalizedString("PREFIX_FOOD_NOTHING", comment: ""), suffix: NSLocalizedString("SUFFIX_FOOD_WELL", comment: "")))
         return dict
     }
     
     private static func evacuation() -> [[String: String]] {
         var dict = [[String: String]]()
-        dict.append(self.getButton("normal", preffix: "minha evacuação foi ", suffix: ""))
-        dict.append(self.getButton("pastosa", preffix: "minha evacuação foi ", suffix: ""))
-        dict.append(self.getButton("diarréia", preffix: "eu tive ", suffix: ""))
+        dict.append(self.getButton(NSLocalizedString("TITLE_EVACUATION_NORMAL", comment: ""), preffix: NSLocalizedString("PREFIX_EVACUATION_NORMAL", comment: ""), suffix: NSLocalizedString("SUFFIX_EVACUATION_NORMAL", comment: "")))
+        dict.append(self.getButton(NSLocalizedString("TITLE_EVACUATION_DOUGHLY", comment: ""), preffix: NSLocalizedString("PREFIX_EVACUATION_DOUGHLY", comment: ""), suffix: NSLocalizedString("SUFFIX_EVACUATION_DOUGHLY", comment: "")))
+        dict.append(self.getButton(NSLocalizedString("TITLE_EVACUATION_DIARRHEA", comment: ""), preffix: NSLocalizedString("PREFIX_EVACUATION_DIARRHEA", comment: ""), suffix: NSLocalizedString("SUFFIX_EVACUATION_DIARRHEA", comment: "")))
         return dict
     }
     
     private static func sleep() -> [[String: String]] {
         var dict = [[String: String]]()
-        dict.append(self.getButton("manhã", preffix: "dormi durante a ", suffix: ""))
-        dict.append(self.getButton("meio-dia", preffix: "dormi ao ", suffix: ""))
-        dict.append(self.getButton("tarde", preffix: "dormi durante a ", suffix: ""))
+        dict.append(self.getButton(NSLocalizedString("TITLE_SLEEP_MORNING", comment: ""), preffix: NSLocalizedString("PREFIX_SLEEP_MORNING", comment: ""), suffix: NSLocalizedString("SUFFIX_SLEEP_MORNING", comment: "")))
+        dict.append(self.getButton(NSLocalizedString("TITLE_SLEEP_NOON", comment: ""), preffix: NSLocalizedString("PREFIX_SLEEP_NOON", comment: ""), suffix: NSLocalizedString("SUFFIX_SLEEP_NOON", comment: "")))
+        dict.append(self.getButton(NSLocalizedString("TITLE_SLEEP_AFTERNOON", comment: ""), preffix: NSLocalizedString("PREFIX_SLEEP_AFTERNOON", comment: ""), suffix: NSLocalizedString("SUFFIX_SLEEP_AFTERNOON", comment: "")))
         return dict
     }
     
     private static func diaper() -> [[String: String]] {
         var dict = [[String: String]]()
-        dict.append(self.getButton("1 vez", preffix: "troquei de fralda ", suffix: ""))
-        dict.append(self.getButton("2 vezes", preffix: "troquei de fralda ", suffix: ""))
-        dict.append(self.getButton("3 ou mais", preffix: "troquei de fralda ", suffix: "vezes"))
+        dict.append(self.getButton(NSLocalizedString("TITLE_DIAPER_ONE", comment: ""), preffix: NSLocalizedString("PREFIX_DIAPER_ONE", comment: ""), suffix: NSLocalizedString("SUFFIX_DIAPER_ONE", comment: "")))
+        dict.append(self.getButton(NSLocalizedString("TITLE_DIAPER_TWO", comment: ""), preffix: NSLocalizedString("PREFIX_DIAPER_TWO", comment: ""), suffix: NSLocalizedString("SUFFIX_DIAPER_TWO", comment: "")))
+        dict.append(self.getButton(NSLocalizedString("TITLE_DIAPER_3ORMORE", comment: ""), preffix: NSLocalizedString("PREFIX_DIAPER_3ORMORE", comment: ""), suffix: NSLocalizedString("SUFFIX_DIAPER_3ORMORE", comment: "")))
         return dict
     }
 }
