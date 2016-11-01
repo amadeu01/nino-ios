dfb0c433068389a28d0a3d3ed55f0fc2b983b8e7
Alfredo <alfredo.cnt@gmail.com>
2016-11-01 11:13:37 -0200

    Added a Default VC to Change Password.

--- Nino/Nino Escola/ChangePasswordViewController.swift
+++ Nino/Nino Escola/ChangePasswordViewController.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ChangePasswordViewController: UIViewController {
-
-    @IBOutlet weak var emailTextField: UITextField!
-    var email: String?
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    @IBOutlet weak var changePasswordButton: UIButton!
+class ChangePasswordViewController: DefaultChangePasswordViewController {
+    @IBOutlet weak var schoolEmailTextField: UITextField!
+    @IBOutlet weak var schoolActivityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var schoolChangePasswordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.emailTextField = self.schoolEmailTextField
+        self.activityIndicator = schoolActivityIndicator
+        self.changePasswordButton = schoolChangePasswordButton
         self.addNinoDefaultBackGround()
         if let txt = self.email {
-            self.emailTextField.text = txt
+            self.emailTextField?.text = txt
         }
     }
 
@@ -28,51 +29,9 @@ class ChangePasswordViewController: UIViewController {
     }
     
     @IBAction func changePasswordAction(sender: UIButton) {
-        self.emailTextField.resignFirstResponder()
-        let alertView = UIAlertController(title: "Erro", message: nil , preferredStyle: .Alert)
-         let okAction = UIAlertAction(title: "Ok", style: .Default, handler: nil)
-        guard let user = self.emailTextField.text else {
-            alertView.message =  "Por favor digite um e-mail válido"
-            alertView.addAction(okAction)
-            self.presentViewController(alertView, animated: true, completion: nil)
-            return
+    self.userDidTapToChangePassword { (success) in
+        if success {
+           self.performSegueWithIdentifier("unwindToLogin", sender: self)
         }
-        self.changePasswordButton.userInteractionEnabled = false
-        self.changePasswordButton.alpha = 0.4
-        self.activityIndicator.startAnimating()
-        self.activityIndicator.hidden = false
-        do {
-            try AccountBO.changePassword(user, completionHandler: { (change) in
-                do {
-                    try change()
-                    alertView.title = "Sucesso!"
-                    alertView.message = "Um e-mail foi enviado para o endereço \(user) com as instruções para o registro de uma nova senha."
-                    let okGoBackAction = UIAlertAction(title: "Ok", style: .Default, handler: { (action) in
-                        self.performSegueWithIdentifier("unwindToLogin", sender: self)
-                    })
-                    alertView.addAction(okGoBackAction)
-                    self.presentViewController(alertView, animated: true, completion: nil)
-                } catch {
-                    alertView.message = "Não foi encontrada nenhuma conta com o e-mail \(user)"
-                    let okAction = UIAlertAction(title: "Ok", style: .Default, handler: nil)
-                    alertView.addAction(okAction)
-                    self.presentViewController(alertView, animated: true, completion: nil)
-                    self.changePasswordButton.userInteractionEnabled = true
-                    self.changePasswordButton.alpha = 1
-                    self.activityIndicator.stopAnimating()
-                    self.activityIndicator.hidden = true
-                }
-            })
-        } catch {
-            alertView.message = "Por favor digite um e-mail válido"
-            alertView.addAction(okAction)
-            self.presentViewController(alertView, animated: true, completion: nil)
-            self.changePasswordButton.userInteractionEnabled = true
-            self.changePasswordButton.alpha = 1
-            self.activityIndicator.stopAnimating()
-            self.activityIndicator.hidden = true
-            //TODO: invalid email
-        }
-    }
-    
+        }    }
 }
